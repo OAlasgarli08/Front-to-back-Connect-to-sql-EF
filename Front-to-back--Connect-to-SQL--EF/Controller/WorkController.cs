267b2edd9e02504eaf84eade32141c8a868c2bc0
Omar Alasagrli <omarta@code.edu.az>
2024-04-28 00:58:04 +0400

    Done

---
+++ Front-to-back--Connect-to-SQL--EF/Controller/WorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Front_to_back__Connect_to_SQL__EF.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
